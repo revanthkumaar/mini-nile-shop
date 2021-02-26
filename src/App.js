4badf71670a8041f1d209610f46dadc8fb058ad0
revanthkumaar <iamrevanthkumar@gmail.com>
2021-02-26 12:40:00 +0530

    delete contact
    delete conatct func added

--- src/App.js
+++ src/App.js
@@ -26,6 +26,11 @@ class App extends Component{
     this.props.createContact(contact);
   }
 
+  deleteContact(e,index){
+    e.preventDefault();
+    this.props.deleteContact(index);
+  }
+
   render(){
     let name;
     return(
@@ -33,17 +38,19 @@ class App extends Component{
         <h1>contacts application</h1>
         <h3>Add contact form</h3>
         <hr/>
-      {/*
+      {
         <ul>
          {this.props.contacts.map((contact,i)=> 
          <li key={i}>{contact.name}</li>)}
         </ul>
 
-      */}
+      }
         <form onSubmit={this.handleSubmit}>
           <input type="text" onChange={this.handleChange}/>
           <input type="submit" />
         </form>
+        <button onclick={this.deleteContact}>Delete Contact</button>
+
       </div>
     );
   }
--- src/reducers/contactReducer.js
+++ src/reducers/contactReducer.js
@@ -2,8 +2,13 @@ import * as actionTypes from '../actions/actionTypes'
 
 export default (state=[], action) => {
     switch(action.type){
+        //to create the contact
         case actionTypes.CREATE_NEW_CONTACT:
             return [...state,Object.assign({},action.contact)];
+        //to delete the contact
+        case actionTypes.REMOVE_CONTACT:
+            return state.filter((data,i) => i !== action.id);
+        //default action is to return the state as is
         default:
             return state; 
     }
