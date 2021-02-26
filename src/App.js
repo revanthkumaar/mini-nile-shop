ebda3fdba8913886ba652906ccd2623fc1858b9f
revanthkumaar <iamrevanthkumar@gmail.com>
2021-02-26 11:07:09 +0530

    checkin
    checkin

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(){
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = {
+      name: ''
+    }
+
+  }
+  handleChange(e){
+    this.setState({
+      name: e.target.value
+    })
+  }
+  handleSubmit(e){
+    e.preventDefault();
+    console.log(this.state.name);
+  }
+
+  render(){
+    let name;
+    return(
+      <div>
+        <h1>contacts application</h1>
+        <h3>Add contact form</h3>
+        <hr/>
+      {/*
+        <ul>
+         {this.props.contacts.map((contact,i)=> 
+         <li key={i}>{contact.name}</li>)}
+        </ul>
+
+      */}
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" onChange={this.handleChange}/>
+          <input type="submit" />
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
